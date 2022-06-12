592a8f7b9965380db43fbd1600c8ec921549930c
lukaszkukla <lukaszkukla@gmail.com>
2022-06-12 16:37:52 +0000

    feat: add company.json file

--- run.py
+++ run.py
@@ -1,26 +1,36 @@
+from dataclasses import dataclass
 import os
+import json
 from flask import Flask, render_template, render_template_string
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
-    return render_template('about.html', page_title='About')
+    data = []
+    with open('data/company.json', 'r') as json_data:
+        data = json.load(json_data)
+    return render_template('about.html', page_title='About', company=data)
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html', page_title='Contact')
 
+
 @app.route('/careers')
 def careers():
     return render_template('careers.html', page_title='Careers')
+
 
 if __name__ == '__main__':
     app.run(
         host=os.environ.get('IP', '0.0.0.0'),
         port=int(os.environ.get('PORT', '5000')),
-        debug=True) # change to False if production version
+        debug=True)  # change to False if production version
